2c0745a50940ba55c06d0c80f8df72dcbe088791
iquach92 <iquach@icloud.com>
2018-08-15 01:52:09 +0300

    12

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 const searchInput = document.getElementById('filter');
 const clearBtn = document.getElementById('clear');
+var blockToAddForm = document.querySelector('.blockAdd');
+var openBtn = document.getElementById('openBtn');
+var cancelBtn = document.getElementById('cancelBtn');
+var addBtn = document.getElementById('addBtn');
+
+
 
 clearBtn.addEventListener('click', function() {
     searchInput.value = '';
+});
+openBtn.addEventListener('click', function () {
+    blockToAddForm.style.display = "block";
+});
+cancelBtn.addEventListener('click', function () {
+    blockToAddForm.style.display = "none";
+    clearForm();
 });
